33ea612c955c63908790eed26c5cfc7acd5fb5a8
earghand <eliasarghand@gmail.com>
2020-04-26 15:33:43 -0700

    Added levels system

--- discordBot.py
+++ discordBot.py
@@ -25,35 +25,35 @@ async def on_member_join(member):
 		json.dump(users, f)
 
 
-# @client.event
-# async def on_message(message):
-# 	with open('users.json', 'r') as f:
-# 		users = json.load(f)
+@client.event
+async def on_message(message):
+	with open('users.json', 'r') as f:
+		users = json.load(f)
 
-# 	await update_data(users, message.author)
-# 	await add_experience(users, message.author, 5)
-# 	await level_up(users, message.author, message.channel)
+	await update_data(users, message.author)
+	await add_experience(users, message.author, 5)
+	await level_up(users, message.author, message.channel)
 
-# 	with open('users.json', 'w') as f:
-# 		json.dump(users, f)
+	with open('users.json', 'w') as f:
+		json.dump(users, f)
 
-# async def update_data(users, user):
-# 	if not user.id in users:
-# 		users[user.id] = {}
-# 		users[user.id]['experience'] = 0
-# 		users[user.id]['level'] = 1
+async def update_data(users, user):
+	if not user.id in users:
+		users[user.id] = {}
+		users[user.id]['experience'] = 0
+		users[user.id]['level'] = 1
 
-# async def add_experience(users, user, exp):
-# 	users[user.id]['experience'] += exp
+async def add_experience(users, user, exp):
+	users[user.id]['experience'] += exp
 
-# async def level_up(users, user, channel):
-# 	experience = users[user.id]['experience']
-# 	lvl_start = users[user.id]['level']
-# 	lvl_end = int(experience ** (1/4))
+async def level_up(users, user, channel):
+	experience = users[user.id]['experience']
+	lvl_start = users[user.id]['level']
+	lvl_end = int(experience ** (1/4))
 
-# 	if lvl_start < lvl_end:
-# 		await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-# 		users[user.id]['level'] = lvl_end
+	if lvl_start < lvl_end:
+		await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
+		users[user.id]['level'] = lvl_end
 
 #Provides output to the console when a new member has joined the server
 @client.event
